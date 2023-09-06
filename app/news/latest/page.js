64895a3319248704e11c42510933c941da2f3e96
voke sokoh <vokes9810@gmail.com>
2023-09-07 03:11:57 +0800

    This is the first commit

---
+++ app/news/latest/page.js
@@ -0,0 +1,10 @@
+const LatestNews = () => {
+    return ( 
+        <div>
+            <h1>Latest News</h1>
+            <p>Just want to be damn good with this shit</p>
+        </div>
+     );
+}
+ 
+export default LatestNews;
---
+++ app/news/page.js
@@ -0,0 +1,10 @@
+const News = () => {
+    return ( 
+        <div>
+            <h1>News</h1>
+            <p>This is the news page bitch</p>
+        </div>
+     );
+}
+ 
+export default News;
